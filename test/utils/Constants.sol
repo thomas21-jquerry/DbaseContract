1a077f6b89233471df18775fdd22bfcb880ac572
Amalnath Sathyan <amalnathsathyan@Amalnaths-MacBook-Pro.local>
2024-09-24 16:04:21 +0530

    solc version chages

--- test/utils/Constants.sol
+++ test/utils/Constants.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.20;
+pragma solidity 0.8.25;
 
 address constant DEPLOYER = address(0x1234123412341234123412341234123412341234);
 address constant ADMIN = address(0x1212121212121212121212121212121212121212);
